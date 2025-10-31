943cec1c7943a4dfc2f20406420e81eeae141bfc
Naincy-Verma <naincyverma0112@gmail.com>
2025-10-31 12:33:57 +0530

    login updated

--- resources/views/admin/pages/login.blade.php
+++ resources/views/admin/pages/login.blade.php
@@ -47,24 +47,11 @@
                                             <label><strong>Password</strong></label>
                                             <input type="password"  name="password" class="form-control" placeholder="Password">
                                         </div>
-                                        <div class="form-row d-flex justify-content-between mt-4 mb-2">
-                                            <div class="form-group">
-                                               <div class="custom-control custom-checkbox ml-1">
-													<input type="checkbox" class="custom-control-input" id="basic_checkbox_1" name="remember">
-													<label class="custom-control-label" for="basic_checkbox_1">Remember my preference</label>
-												</div>
-                                            </div>
-                                            <div class="form-group">
-                                                <a href="">Forgot Password?</a>
-                                            </div>
-                                        </div>
+
                                         <div class="text-center">
                                             <button type="submit" class="btn btn-primary btn-block">Login</button>
                                         </div>
                                     </form>
-                                    <div class="new-account mt-3">
-                                        <p>Don't have an account? <a class="text-primary" href="">Sign up</a></p>
-                                    </div>
                                 </div>
                             </div>
                         </div>
